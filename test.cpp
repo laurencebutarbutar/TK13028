9dabff212eb797e81fad7d96f30558b2578e1142
laurencebutarbutar <laurencebutarbutar@gmail.com>
2024-09-05 17:32:37 +0700

    test pertama

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size = 10;
+    for (int index = 1; index <= size; index++)
+    {
+        cout << "Program C++ " << "ke-" << index << endl;
+    }
+    
+    return 0;
+}
