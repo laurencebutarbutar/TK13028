1f5d11c2251d336e1f930c0619c822154b726928
laurencebutarbutar <laurencebutarbutar@gmail.com>
2024-09-05 17:33:29 +0700

    test upload

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ int main()
     int size = 10;
     for (int index = 1; index <= size; index++)
     {
-        cout << "Program C++ " << "ke-" << index << endl;
+        cout << "Test " << "ke-" << index << endl;
     }
     
     return 0;
